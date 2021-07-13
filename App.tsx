fec7fe260fa2f355e2972f9cc5cdb190951c9f71
brennop <brennopcordeiro@gmail.com>
2021-07-13 11:51:12 -0300

    🎉 Aula 1

--- App.tsx
+++ App.tsx
@@ -1,21 +1,46 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+import { useFonts } from 'expo-font';
+import {
+  Inter_400Regular,
+  Inter_500Medium,
+} from "@expo-google-fonts/inter";
+import {
+  Barlow_300Light_Italic,
+  Barlow_500Medium,
+  Barlow_600SemiBold,
+  Barlow_700Bold,
+} from "@expo-google-fonts/barlow";
+import AppLoading from 'expo-app-loading';
+
+import Feed from "./src/pages/Feed";
 
 export default function App() {
+  let [loaded] = useFonts({
+    Inter_400Regular,
+    Inter_500Medium,
+    Barlow_300Light_Italic,
+    Barlow_500Medium,
+    Barlow_600SemiBold,
+    Barlow_700Bold,
+  });
+  
+  if (!loaded) return <AppLoading />;
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <>
+      <Feed />
+      <StatusBar style="dark" backgroundColor="#fff" translucent={false} />
+    </>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
---
+++ src/types/global.d.ts
@@ -0,0 +1,10 @@
+
+type Pokemon = {
+    id: number;
+    name: string;
+    image_url: string;
+    number: number;
+    weight: number;
+    height: number;
+    kind: string;
+}
